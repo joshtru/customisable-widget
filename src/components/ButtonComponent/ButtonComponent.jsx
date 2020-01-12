3b22680303e019fd804a0a0058d8b8717146236e
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2020-01-12 15:35:55 -0600

    Added prop types checker

--- src/components/ButtonComponent/ButtonComponent.jsx
+++ src/components/ButtonComponent/ButtonComponent.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 // IMPORTING REDUX ACTION
 import { toggleModal } from "../../redux/modal/modal.action";
 
-import styles from "./buttonComponent.module.css";
-
-const ButtonComponent = ({ text, openModal, toggleModal }) => (
-  <button className={styles.button} type="button" onClick={() => toggleModal()}>
-    {openModal ? "Hide Modal" : `${text}`}
+const ButtonComponent = ({ text, openModal, toggleModal, triggerModal }) => (
+  <button type="button" onClick={() => (triggerModal ? toggleModal() : null)}>
+    {text}
   </button>
 );
+// Define prop types
+ButtonComponent.defaultProps = {
+  triggerModal: true
+};
+ButtonComponent.propTypes = {
+  text: PropTypes.string,
+  openModal: PropTypes.bool,
+  triggerModal: PropTypes.bool
+};
+// Get state from redux
 const mapStateToProps = ({ modal }) => ({
   openModal: modal.openModal
 });
+// Dispatch redux action
 const mapDispatchToProps = dispatch => ({
   toggleModal: () => dispatch(toggleModal())
 });
