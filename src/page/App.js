ae3cac054b0a01d8031a0cdd2a4a4971fdeefd4c
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2020-01-12 15:37:06 -0600

    Added more sample data

--- src/page/App.js
+++ src/page/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "./App.module.css";
-import Data from "../data/data2";
+import Data from "../data/data4";
 import GetComponent from "../components/GetComponent/GetComponent";
 import ErrorBoundary from "../components/ErrorBoundary/ErrorBoundary.component.jsx";
 
 const App = () => {
   return (
     <ErrorBoundary>
-    <main className={styles.App}>
-      <GetComponent data={Data} />
-    </main>
+      <main className={styles.App}>
+        <GetComponent data={Data} />
+      </main>
     </ErrorBoundary>
   );
 };
