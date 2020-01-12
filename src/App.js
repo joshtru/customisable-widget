7e40e87d4054228421b19b351db2e8560338fedd
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2020-01-12 12:13:34 -0600

    Dynamically rending component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css";
+import Data from "./data/data2";
+import GetComponent from "./components/GetComponent/GetComponent";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={styles.App}>
+      <GetComponent data={Data} />
+    </main>
   );
-}
+};
 
 export default App;
