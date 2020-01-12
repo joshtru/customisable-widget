347b407932a4fc4c6b37122b3e15bbd89055f4e0
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2020-01-12 15:34:25 -0600

    Added Error boundary

--- src/page/App.js
+++ src/page/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import styles from "./App.module.css";
 import Data from "../data/data2";
 import GetComponent from "../components/GetComponent/GetComponent";
+import ErrorBoundary from "../components/ErrorBoundary/ErrorBoundary.component.jsx";
 
 const App = () => {
   return (
+    <ErrorBoundary>
     <main className={styles.App}>
       <GetComponent data={Data} />
     </main>
+    </ErrorBoundary>
   );
 };
 
