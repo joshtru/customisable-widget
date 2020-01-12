10ea14df514edd400480d4b1b357c209179fa135
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2020-01-12 15:36:40 -0600

    Added toggle modal functionality

--- src/components/LinkComponent/LinkComponent.jsx
+++ src/components/LinkComponent/LinkComponent.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+// IMPORTING REDUX ACTION
+import { toggleModal } from "../../redux/modal/modal.action";
 
-const LinkComponent = ({ text, url }) => (
-  <a
-    target="_blank"
-    rel="noopener noreferrer"
-    href={`${url}`}
-    title={`${text}`}
-  >
-    {text}
-  </a>
+const LinkComponent = ({ text, url, triggerModal, toggleModal }) =>
+  text ? (
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href={`${url}`}
+      title={`${text}`}
+      onClick={() => (triggerModal ? toggleModal() : null)}
+    >
+      {text}
+    </a>
+  ) : null;
 // Define prop types
 LinkComponent.defaultProps = {
   triggerModal: false,
@@ -19,3 +26,9 @@ LinkComponent.propTypes = {
   url: PropTypes.string,
   triggerModal: PropTypes.bool
 };
+
+// Dispatch redux action
+const mapDispatchToProps = dispatch => ({
+  toggleModal: () => dispatch(toggleModal())
+});
+export default connect(null, mapDispatchToProps)(LinkComponent);
